58cc270d533e8084d1f379860f4e5e96d128c5fa
Kenshu Hori <37034086+kenshuhori@users.noreply.github.com>
2023-12-16 13:56:24 +0900

    数値を受けとたったら2倍にして返す。数値でなければerrorを返す (#3)

---
+++ workspace/atcoderA/src/main.rs
@@ -0,0 +1,15 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        value: String,
+    }
+    let num: i32 = match value.parse::<i32>() {
+      Ok(n) => n,
+      Err(_) => {
+          println!("error");
+          return
+      },
+    };
+    println!("{}", num * 2);
+}

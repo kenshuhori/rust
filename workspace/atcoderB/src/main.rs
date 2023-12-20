bc00fc3e5da4f8cb7294f85064e604da99206b9e
Kenshu Hori <37034086+kenshuhori@users.noreply.github.com>
2023-12-20 21:27:20 +0900

    Apply suggestions from code review

--- workspace/atcoderB/src/main.rs
+++ workspace/atcoderB/src/main.rs
@@ -1,8 +1,19 @@
 use proconio::input;
 
-// input
+// input例
 // 11
 // 10 9 10 3 100 100 90 80 10 30 10
+// output例
+// down 1
+// up 1
+// down 7
+// up 97
+// stay
+// down 10
+// down 10
+// down 70
+// up 20
+// down 20
 
 fn main() {
     let mut last_earning: Option<i32> = None;

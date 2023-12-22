6c8797652057b12212f7c3465bb5573fc9d3f860
Kenshu Hori <37034086+kenshuhori@users.noreply.github.com>
2023-12-22 21:28:29 +0900

    3番目に大きい数字 (#5)

--- workspace/atcoder_c/src/main.rs
+++ workspace/atcoder_c/src/main.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
+// 問題
+// 3番目に大きい数字を出力する
+
+// input例
+//4 18 25 20 9 13
+
+// output例
+// 18
+
 fn main() {
-    println!("Hello, world!");
+    input! {
+        mut numbers: [i32; 6],
+    }
+
+    numbers.sort_by(|a, b| a.cmp(b));
+    println!("{}", numbers[3])
 }

63c7590fc48ce55ddce6a306831f5a48b347471c
Kenshu Hori <37034086+kenshuhori@users.noreply.github.com>
2023-12-22 20:52:10 +0900

    [atcoder] 前日売上との差分を出す問題 (#4)
    * [atcoder] 前日売上との差分を出す問題
    
    * Apply suggestions from code review
    
    * Update workspace/atcoderB/src/main.rs

---
+++ workspace/atcoderB/src/main.rs
@@ -0,0 +1,43 @@
+use proconio::input;
+
+// input例
+// 11
+// 10 9 10 3 100 100 90 80 10 30 10
+
+// output例
+// down 1
+// up 1
+// down 7
+// up 97
+// stay
+// down 10
+// down 10
+// down 70
+// up 20
+// down 20
+
+fn main() {
+    let mut last_earning: Option<i32> = None;
+
+    input! {
+        n: usize,
+        earnings: [i32; n],
+    }
+
+    for earning in earnings {
+        if last_earning == None {
+            last_earning = Some(earning);
+            continue;
+        }
+
+        if earning == last_earning.unwrap() {
+            println!("stay");
+        } else if earning > last_earning.unwrap() {
+            println!("up {}", earning - last_earning.unwrap());
+        } else {
+            println!("down {}", last_earning.unwrap() - earning);
+        }
+
+        last_earning = Some(earning);
+    }
+}

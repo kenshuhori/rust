c150aa0e1f20da8ec1ee1d53ec379d5d1b87c92d
Kenshu Hori <kenshuhori@gmail.com>
2025-08-17 16:51:07 +0900

    from 実装

--- workspace/dive_from_trait/src/main.rs
+++ workspace/dive_from_trait/src/main.rs
@@ -9,6 +9,12 @@ struct Person {
 #[derive(Debug)]
 struct Age(u8);
 
+impl From<u8> for Age {
+    fn from(value: u8) -> Self {
+        Age(value)
+    }
+}
+
 fn main() {
     let age_value = 35_u8;
     let yoshida = Person {

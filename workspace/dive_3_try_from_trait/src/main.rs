daf45f531cfe73f6c9037244f591b1c2bc98c92a
Kenshu Hori <kenshuhori@gmail.com>
2025-08-23 22:40:16 +0900

    tryfrom実装

--- workspace/dive_3_try_from_trait/src/main.rs
+++ workspace/dive_3_try_from_trait/src/main.rs
@@ -1,24 +1,34 @@
 #[derive(Debug)]
 pub struct Person {
-    pub nickname: String,
-    pub age: Age,
+    nickname: String,
+    age: Age,
 }
 
 #[derive(Debug)]
 struct Age(u8);
 
-impl From<u8> for Age {
-    fn from(value: u8) -> Self {
-        Age(value)
+#[derive(Debug)]
+enum AgeError {
+    Impossible,
+}
+
+impl TryFrom<u8> for Age {
+    type Error = AgeError;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        if value > 130 {
+            return Err(AgeError::Impossible);
+        }
+        Ok(Age(value))
     }
 }
 
 fn main() {
-    let age_value = 35_u8;
+    let age_value = 135_u8;
     let nickname_value = "yosshi-";
     let yoshida = Person {
         nickname: String::from(nickname_value),
-        age: Age::try_from(age_value),
+        age: Age::try_from(age_value).expect("年齢の変換は成功するはず"),
     };
     assert_eq!(yoshida.nickname, nickname_value);
     assert_eq!(yoshida.age.0, age_value);

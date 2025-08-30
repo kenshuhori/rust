69d0dc5eeded1812f17ce7e874cab56da0fbd63e
Kenshu Hori <kenshuhori@gmail.com>
2025-08-31 00:29:20 +0900

    serde利用

--- workspace/dive_4_serde_crate/src/main.rs
+++ workspace/dive_4_serde_crate/src/main.rs
@@ -1,3 +1,19 @@
+#[derive(serde::Serialize, serde::Deserialize)]
+struct Person {
+    nickname: String,
+    age: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let person = Person {
+        nickname: String::from("タロー"),
+        age: 30,
+    };
+
+    let serialized = serde_json::to_string(&person).unwrap();
+    let deserialized: Person = serde_json::from_str(&serialized).unwrap();
+
+    assert_eq!(serialized, "{\"nickname\":\"タロー\",\"age\":30}");
+    assert_eq!(person.nickname, deserialized.nickname);
+    assert_eq!(person.age, deserialized.age);
 }

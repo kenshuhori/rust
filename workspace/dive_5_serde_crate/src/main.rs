36669c5be16763c8a6fc50b44fa69cafe023ef44
Kenshu Hori <kenshuhori@gmail.com>
2025-09-06 20:18:19 +0900

    執筆

--- workspace/dive_5_serde_crate/src/main.rs
+++ workspace/dive_5_serde_crate/src/main.rs
@@ -1,34 +1,33 @@
-#[derive(serde::Serialize, serde::Deserialize)]
+#[derive(serde::Serialize)]
 struct Person {
-    name: Name,
-    age: Age,
-}
-
-#[derive(serde::Serialize, serde::Deserialize, PartialEq, Debug)]
-struct Name {
     first_name: String,
     last_name: String,
+    age: Age,
+    gender: Gender,
 }
 
 #[derive(serde::Serialize, serde::Deserialize, PartialEq, Debug)]
 struct Age(u8);
 
+#[derive(serde::Serialize, serde::Deserialize, PartialEq, Debug)]
+enum Gender {
+    Male,
+    Female,
+    Other,
+}
+
 fn main() {
     let person = Person {
-        name: Name {
-            first_name: String::from("太郎"),
-            last_name: String::from("田中"),
-        },
+        first_name: String::from("太郎"),
+        last_name: String::from("田中"),
         age: Age(30),
+        gender: Gender::Male,
     };
 
     let serialized = serde_json::to_string(&person).unwrap();
-    let deserialized: Person = serde_json::from_str(&serialized).unwrap();
 
     assert_eq!(
         serialized,
-        "{\"name\":{\"first_name\":\"太郎\",\"last_name\":\"田中\"},\"age\":30}"
+        "{\"first_name\":\"太郎\",\"last_name\":\"田中\",\"age\":30,\"gender\":\"Male\"}"
     );
-    assert_eq!(person.name, deserialized.name);
-    assert_eq!(person.age, deserialized.age);
 }

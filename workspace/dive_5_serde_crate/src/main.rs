9036d750525e6a4f522b91ababc47be679c80137
Kenshu Hori <kenshuhori@gmail.com>
2025-09-06 19:11:39 +0900

    Person構造体とName構造体とAge構造体を利用して検証

--- workspace/dive_5_serde_crate/src/main.rs
+++ workspace/dive_5_serde_crate/src/main.rs
@@ -1,19 +1,34 @@
 #[derive(serde::Serialize, serde::Deserialize)]
 struct Person {
-    nickname: String,
-    age: u8,
+    name: Name,
+    age: Age,
 }
+
+#[derive(serde::Serialize, serde::Deserialize, PartialEq, Debug)]
+struct Name {
+    first_name: String,
+    last_name: String,
+}
+
+#[derive(serde::Serialize, serde::Deserialize, PartialEq, Debug)]
+struct Age(u8);
 
 fn main() {
     let person = Person {
-        nickname: String::from("タロー"),
-        age: 30,
+        name: Name {
+            first_name: String::from("太郎"),
+            last_name: String::from("田中"),
+        },
+        age: Age(30),
     };
 
     let serialized = serde_json::to_string(&person).unwrap();
     let deserialized: Person = serde_json::from_str(&serialized).unwrap();
 
-    assert_eq!(serialized, "{\"nickname\":\"タロー\",\"age\":30}");
-    assert_eq!(person.nickname, deserialized.nickname);
+    assert_eq!(
+        serialized,
+        "{\"name\":{\"first_name\":\"太郎\",\"last_name\":\"田中\"},\"age\":30}"
+    );
+    assert_eq!(person.name, deserialized.name);
     assert_eq!(person.age, deserialized.age);
 }

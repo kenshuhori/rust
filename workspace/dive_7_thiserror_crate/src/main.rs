9ed3213da706a5822654e9d4e6e1791548b2a305
Kenshu Hori <37034086+kenshuhori@users.noreply.github.com>
2026-01-16 22:27:56 +0900

    person_with_std_error を追加

--- workspace/dive_7_thiserror_crate/src/main.rs
+++ workspace/dive_7_thiserror_crate/src/main.rs
@@ -1,9 +1,11 @@
 mod person;
+mod person_with_std_error;
 
-use person::Person;
+// use person::Person;
+// use person_with_std_error::Person;
 
 fn main() {
-    let person_with_invalid_nickname = Person::new(
+    let person_with_invalid_nickname = person_with_std_error::Person::new(
         "",
         30,
     );
@@ -17,7 +19,7 @@ fn main() {
         }
     }
 
-    let person_with_invalid_age = Person::new(
+    let person_with_invalid_age = person_with_std_error::Person::new(
         "Alice",
         -50,
     );

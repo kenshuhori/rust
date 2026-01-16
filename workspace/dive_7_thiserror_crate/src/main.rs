54642ca4a305e30371ca34fa3eec9df59f8812af
Kenshu Hori <37034086+kenshuhori@users.noreply.github.com>
2026-01-16 21:21:18 +0900

    Update main.rs

--- workspace/dive_7_thiserror_crate/src/main.rs
+++ workspace/dive_7_thiserror_crate/src/main.rs
@@ -3,5 +3,31 @@ mod person;
 use person::Person;
 
 fn main() {
-    println!("Hello, world!");
+    let person_with_invalid_nickname = Person::new(
+        "",
+        30,
+    );
+
+    match person_with_invalid_nickname {
+        Ok(_) => {
+            println!("Person created successfully.");
+        }
+        Err(e) => {
+            println!("Error: {}", e);
+        }
+    }
+
+    let person_with_invalid_age = Person::new(
+        "Alice",
+        -50,
+    );
+
+    match person_with_invalid_age {
+        Err(e) => {
+            println!("Error: {}", e);
+        }
+        Ok(_) => {
+            println!("Person created successfully.");
+        }
+    }
 }

dfdee13b17e662c8773ec7307496402dc73498f3
Kenshu Hori <37034086+kenshuhori@users.noreply.github.com>
2026-03-06 21:28:16 +0900

    Update main.rs

--- workspace/dive_9_anyhow_crate/src/main.rs
+++ workspace/dive_9_anyhow_crate/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    match read_file_ext() {
+        Ok(_) => println!("成功"),
+        Err(e) => println!("エラー: {e}"),
+    }
+}
+
+fn read_file_ext() -> anyhow::Result<String> {
+    let file = read_file()?;
+    Ok(file)
+}
+
+fn read_file() -> std::result::Result<String, std::io::Error> {
+    std::fs::read_to_string("not_found.txt")
 }

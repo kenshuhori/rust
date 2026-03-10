d78cfe9ca66fb6d6f3e65da392ef7ed8bb53733e
Kenshu Hori <37034086+kenshuhori@users.noreply.github.com>
2026-03-10 09:36:43 +0900

    example

--- workspace/dive_9_anyhow_crate/src/main.rs
+++ workspace/dive_9_anyhow_crate/src/main.rs
@@ -1,18 +1,7 @@
+mod example1;
+mod example2;
+
 fn main() {
-    match read_file_ext() {
-        Ok(_) => println!("成功"),
-        Err(e) => println!("エラー: {e}"),
-    }
-}
-
-fn read_file_ext() -> anyhow::Result<String> {
-    let text = match read_file() {
-        Ok(text) => text,
-        Err(e) => return Err(anyhow::anyhow!(e)),
-    };
-    Ok(text)
-}
-
-fn read_file() -> std::result::Result<String, std::io::Error> {
-    std::fs::read_to_string("not_found.txt")
+    example1::example1();
+    example2::example2();
 }

9d3e4a7d12b190bf88fc10152cd3b6ff8c7b9c6c
Kenshu Hori <37034086+kenshuhori@users.noreply.github.com>
2026-03-10 09:05:41 +0900

    Update main.rs

--- workspace/dive_9_anyhow_crate/src/main.rs
+++ workspace/dive_9_anyhow_crate/src/main.rs
@@ -6,8 +6,11 @@ fn main() {
 }
 
 fn read_file_ext() -> anyhow::Result<String> {
-    let file = read_file()?;
-    Ok(file)
+    let text = match read_file() {
+        Ok(text) => text,
+        Err(e) => return Err(anyhow::anyhow!(e)),
+    };
+    Ok(text)
 }
 
 fn read_file() -> std::result::Result<String, std::io::Error> {

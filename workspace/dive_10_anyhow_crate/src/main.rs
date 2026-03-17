a1560418d8a8de5f61eb5dd98fce4c3c719fe896
Kenshu Hori <kenshuhori@gmail.com>
2026-03-17 21:19:11 +0900

    fi

--- workspace/dive_10_anyhow_crate/src/main.rs
+++ workspace/dive_10_anyhow_crate/src/main.rs
@@ -1,5 +1,16 @@
-mod example1;
-
 pub(crate) fn main() {
-    example1::example1();
+    match read_file_ext() {
+        Ok(_) => println!("成功"),
+        Err(e) => println!("エラー: {e}"),
+    }
+}
+
+fn read_file_ext() -> anyhow::Result<String> {
+    #[warn(deprecated)]
+    let text = r#try!(read_file());
+    Ok(text)
+}
+
+fn read_file() -> std::result::Result<String, std::io::Error> {
+    std::fs::read_to_string("not_found.txt")
 }

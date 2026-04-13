145761feaffc309a3b3ddaeeb34e6679d7e6256a
Kenshu Hori <kenshuhori@gmail.com>
2026-04-13 20:43:54 +0900

    コード例を追加

--- workspace/dive_12_tracing_crate/src/main.rs
+++ workspace/dive_12_tracing_crate/src/main.rs
@@ -1,9 +1,26 @@
-use tracing::{info, warn};
+use tracing::{Level, event, span};
 use tracing_subscriber;
 
 fn main() {
-    tracing_subscriber::fmt::init();
+    let subscriber = tracing_subscriber::fmt::Subscriber::new();
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("failed to set global default subscriber");
 
-    info!("Hello tracing!");
-    warn!("Something might be wrong");
+    event!(Level::INFO, "hello, tracing!");
+
+    let span = span!(Level::INFO, "main_span");
+    let _enter = span.enter();
+
+    event!(Level::INFO, "inside span");
+
+    sub();
+
+    event!(Level::INFO, "back in main span");
+}
+
+fn sub() {
+    event!(Level::WARN, "inside sub function");
+    let span = span!(Level::INFO, "sub_span");
+    let _enter = span.enter();
+    event!(Level::ERROR, "inside sub span");
 }

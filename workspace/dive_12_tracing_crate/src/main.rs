09c3b6f5ce38b9113b5a619444f27931aa6ccc60
Kenshu Hori <37034086+kenshuhori@users.noreply.github.com>
2026-04-13 23:26:46 +0900

    dive_12_tracing_crate を追加 (#15)
    * dive_12_tracing_crate を追加
    
    * Update Cargo.toml
    
    * Update Cargo.toml
    
    * Update main.rs
    
    * コード例を追加
    
    * Update main.rs
    
    * Update main.rs
    
    * Update main.rs
    
    * instrumentを試す
    
    * Update main.rs
    
    * Update main.rs

---
+++ workspace/dive_12_tracing_crate/src/main.rs
@@ -0,0 +1,23 @@
+use tracing::{Level, event, instrument, span};
+use tracing_subscriber;
+
+fn main() {
+    let subscriber = tracing_subscriber::fmt::Subscriber::new();
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("failed to set global default subscriber");
+
+    event!(Level::INFO, "hello, tracing!");
+    let span = span!(Level::INFO, "main");
+    let _enter = span.enter();
+    event!(Level::INFO, "inside span");
+    sub(42);
+    event!(Level::INFO, "back in main span");
+}
+
+#[instrument]
+fn sub(user_id: u64) {
+    // let span = span!(Level::INFO, "sub", user_id = user_id);
+    // let _enter = span.enter();
+    event!(Level::WARN, "inside sub function");
+    event!(Level::ERROR, "inside sub span");
+}

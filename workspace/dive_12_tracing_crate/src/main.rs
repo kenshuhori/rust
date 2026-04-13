34a732f75c8a7dffa47cd1fe76bf36c93bbaab7c
Kenshu Hori <kenshuhori@gmail.com>
2026-04-13 20:51:28 +0900

    Update main.rs

--- workspace/dive_12_tracing_crate/src/main.rs
+++ workspace/dive_12_tracing_crate/src/main.rs
@@ -7,22 +7,16 @@ fn main() {
         .expect("failed to set global default subscriber");
 
     event!(Level::INFO, "hello, tracing!");
-
     let span = span!(Level::INFO, "main_span");
     let _enter = span.enter();
-
     event!(Level::INFO, "inside span");
-
     sub();
-
     event!(Level::INFO, "back in main span");
 }
 
 fn sub() {
     event!(Level::WARN, "inside sub function");
-
     let span = span!(Level::INFO, "sub_span");
     let _enter = span.enter();
-
     event!(Level::ERROR, "inside sub span");
 }
